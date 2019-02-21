b5371a190a67f3705fe8d73a7906069375139728
Laura <laura.robertson64@gmail.com>
2019-02-21 15:49:15 -0600

    proxy talks to description service

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+const Request = require('request')
 const express = require('express')
+const Axios = require('axios')
 const app = express()
 
-app.use(express.static('./'))
+app.use('/', express.static('./'))
 
 app.get('/', (req, res) => {
-  //db.seed();
   res.send('index.html')
 })
-//this could be a proxy server, it needs to ge the request from my services server
-//need to set that up, define an api, which will grab data from the database
 
-app.listen(5000, () => console.log('Server running on port 5000'))
+app.get('/desc', (req, res) => {
+  Axios.get(`http://interstellar-desc.us-east-2.elasticbeanstalk.com/desc`)
+  .then((result) => {res.send(result.data)})
+  .catch(() => console.log('it did not work'))
+})
+
+app.get('/desc/:key', (req, res) => {
+  let key = req.params.key;
+  Axios.get(`http://interstellar-desc.us-east-2.elasticbeanstalk.com/desc/${key}`)
+  .then((result) => {res.send(result.data)})
+  .catch(() => console.log('it did not work'))
+})
+
+app.get('/pic/:picKey', (req, res) => {
+  let picKey = req.params.picKey;
+  Axios.get(`http://interstellar-desc.us-east-2.elasticbeanstalk.com/pic/${picKey}`)
+  .then((result) => {res.send(result.data)})
+  .catch(() => console.log('it did not work'))
+})
+
+app.listen(4001, () => console.log('Server running on port 4001'))
